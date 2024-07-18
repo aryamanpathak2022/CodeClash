c35235fed0374b2fa8deab9ff0e70e6c3e388193
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-07-18 15:58:18 +0530

    Update

---
+++ Codeclash/apps/web/app/main/problems/page.js
@@ -0,0 +1,15 @@
+
+import Image from "next/image";
+import MainPage from "../../../@/components/component/main-page"
+
+
+export default function app() {
+  return (
+    
+    <div >
+        <MainPage />
+   
+    </div>
+
+  );
+}
--- Codeclash/apps/web/app/qst/page.js
+++ Codeclash/apps/web/app/qst/page.js
@@ -1,30 +1,49 @@
-
 "use client"
 import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import CodeMirror from "@uiw/react-codemirror";
-import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import Editor from "@monaco-editor/react";
 
 const Home = () => {
   const [markdownText, setMarkdownText] = useState('# Hello, Markdown!');
- const code = "console.log('Code Mirror!');"
-  const handleCodeChange = (e) => {
-    setMarkdownText(e.target.value);
+  const [code, setCode] = useState("console.log('Code Mirror!');");
+  const [selectedLanguage, setSelectedLanguage] = useState('javascript');
+
+  const handleCodeChange = (newValue) => {
+    setCode(newValue);
+  };
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
   };
 
   return (
-    <div className="container flex ">
-      <div className="panel markdown-panel w-1/2">
+    <div className="container flex">
+      <div className="panel markdown-panel w-full md:w-1/2">
         <ReactMarkdown>{markdownText}</ReactMarkdown>
       </div>
-      <div className="panel code-panel w-1/2">
-      <CodeMirror
-      value={code}
-      height="100vh"
-
-      theme={vscodeDark}
-    />
-    </div>
+      <div className="panel code-panel w-full md:w-1/2">
+        <div className="flex justify-between items-center">
+          <select
+            className="px-2 py-1 border border-gray-300 rounded"
+            value={selectedLanguage}
+            onChange={handleLanguageChange}
+          >
+            <option value="javascript">JavaScript</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+            <option value="cpp">C++</option>
+          </select>
+         
+        </div>
+        <Editor
+          height="70vh"
+          defaultLanguage={selectedLanguage}
+          defaultValue={code}
+          onChange={handleCodeChange}
+          theme="vs-dark"
+        />
+         <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Submit</button>
+      </div>
     </div>
   );
 };
